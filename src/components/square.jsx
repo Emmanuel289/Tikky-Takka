b763f4bdf0fc74106ee1ead98eea55636143f869
Okechukwu Ochia <emmirald@gmail.com>
2020-04-13 02:06:16 -0600

    click handler inserts an X when a square is clicked

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
 
-export default class Square extends Component {
+class Square extends Component {
+  constructor(props) {
+    super(props); //call constructor of Square and pass in its props
+    this.state = {
+      value: null, //initialize value to null
+    };
+  }
+
+  fillWithX = () => {
+    this.setState({ value: "X" }); //change value from null to X when a square is clicked
+  };
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={this.fillWithX}>
+        {this.state.value}
+      </button>
+    ); //value prop referenced from Board
   }
 }
+export default Square;
